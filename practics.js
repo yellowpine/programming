5637eda7a7adc4449b79279b1dd270fcd354e180
Igor <hello.yellowpine@gmail.com>
2020-04-08 16:21:41 +0300

    task_1

---
+++ practics.js
@@ -0,0 +1,22 @@
+
+console.log(leftBorderWidth);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log('string'*9);
+
+let person = {
+    name: "Igor",
+    age: 31,
+    job: false
+};
+console.log (person.name);
+
+let inc = 10,
+    dec = 10;
+
+inc++;
+dec--;
+
+console.log(inc);
+console.log(dec);
--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 'use strict';
 
-a = 5;
-console.log(a);
+//домашка 1
+let money;
+let time;
 
-console.log(leftBorderWidth);
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-console.log('string'*9);
+var appData = {
+    moneyData: "money",
+    timeData: "time",
+    expenses : {
+        "questionOne" : "answerOne "
+    },
+    optionalExpenses : {},
+    income : [],
+    savings: false
+}
 
-let person = {
-    name: "Igor",
-    age: 31,
-    job: false
-};
-console.log (person.name);
+let questionOne = prompt("Введите обязательную статью расходов в этом месяце");
+let answerOne = prompt("Во сколько обойдется?");
 
-let inc = 10,
-    dec = 10;
-
-inc++;
-dec--;
-
-console.log(inc);
-console.log(dec);
+alert("Бюджет на 1 день " + answerOne/30 +"единица");
